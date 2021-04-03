9075a6866065985bb0b764913faff7954dd49fe3
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2021-04-03 15:57:34 -0500

    Add files via upload

---
+++ read_error_location.m
@@ -0,0 +1,18 @@
+function [data_res]=read_error_location(path_file)
+file=fopen(path_file,'r');
+q=1;
+while(feof(file)~=1)
+    data_n=fgets(file);
+    data_k=strsplit(data_n,',');
+    p=1;
+    for n=1:length(data_k)
+        if data_k{n}(1)=='t'
+            data_res(q,p)=str2num(data_k{n}(8));
+            p=p+1;
+        end;
+    end;
+    q=q+1;
+end;
+data_res=1-data_res;
+fclose(file); 
+A=1;
