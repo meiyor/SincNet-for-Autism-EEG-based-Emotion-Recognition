f3ff2f5cfd1b0cf2d98981bf7dd119ea14f86538
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2023-06-22 04:42:15 -0500

    Update read_filters_act_more.m

--- m_files/read_filters_act_more.m
+++ m_files/read_filters_act_more.m
@@ -17,7 +17,7 @@ if sel==1
         for i=1:399 %% possible training epochs
             %%ask permission for the filter_vals_folder to Juan Manuel Mayor-Torres
             if exist(['filters_act/1730006_filters_act/filters_vals_' num2str(i) '_' subject_code '_' num2str(k) '_more_filters_n.txt'])
-                Mdata{k,q}=dlmread(['filters_act/1730006_filters_act/filters_vals_' num2str(i) '_' subject_code '_' num2str(k) '_more_filters_n.txt'],',');
+                Mdata{k,q}=dlmread(['filters_act/1730006_filters_act/filters_vals_' num2str(i) '_' subject_code '_' num2str(k) '_more_filters.txt'],',');
                 temp_val=zeros([d_size 2000]);
                 for p=1:d_size
                     %temp_val(p,:)=freqz(Mdata{k,q}(p,:),1,1000);
